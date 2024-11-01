ef17cc5d19317d715eaa07235bba9f5a47c2ced7
raipen <raipen@knu.ac.kr>
2024-11-01 20:34:22 +0900

    feat: add global prefix and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swagger } from './swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
+  
 
   if(process.env.NODE_ENV === 'development') {
     swagger(app);
