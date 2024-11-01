db07ae26815127be1162395e3e24a04d362baff1
raipen <raipen@knu.ac.kr>
2024-11-02 05:08:51 +0900

    feat: add properties to User entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({ timestamps: true, versionKey: false })
+export class User {
+    @Prop({ type: String, required: true })
+    uid: string;
+    
+    @Prop({ type: String })
+    email: string;
+    
+    @Prop({ type: String })
+    user_name: string;
+    
+    @Prop({ type: String })
+    user_image: string;
+}
+
+export type UserDocument = HydratedDocument<User>;
+export const UserSchema = SchemaFactory.createForClass(User);
