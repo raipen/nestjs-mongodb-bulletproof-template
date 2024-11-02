30eb1cce423ec6fd8c667076cd1304745e6f4c1a
raipen <raipen@knu.ac.kr>
2024-11-02 20:54:33 +0900

    feat: add AuthService and export it

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class AuthService {
+    
+    constructor(private readonly configService: ConfigService) {
+
+    }
+}
