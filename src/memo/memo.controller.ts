168a3570f4e5f1906063929243b1f7c6d34c9ec1
raipen <raipen@knu.ac.kr>
2024-11-05 03:33:16 +0900

    feat: add Swagger annotations to MemoController CR api

--- src/memo/memo.controller.ts
+++ src/memo/memo.controller.ts
@@ -1,19 +1,28 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { MemoService } from './memo.service';
 import { RequestUpdateMemoNameDto, RequestUpdateMemoDescriptionDto } from './dto/update-memo.dto';
 import { User } from 'src/user/entities/user.entity';
 import { GetUser } from 'src/auth/decorators/user';
+import { Memo } from './entities/memo.entity';
 
 @Controller('memo')
+@ApiTags('Memo')
+@ApiUnauthorizedResponse({ description: 'Unauthorized' })
+@ApiBearerAuth()
 export class MemoController {
   constructor(private readonly memoService: MemoService) {}
 
   @Post()
+  @ApiOperation({ summary: '메모 생성' })
+  @ApiCreatedResponse({ description: '메모 생성', type: String })
   create(@GetUser() user: User) {
     return this.memoService.create(user.id);
   }
 
   @Get()
+  @ApiOperation({ summary: '모든 메모 조회' })
+  @ApiOkResponse({ description: '모든 메모 조회', type: [Memo] })
   findAll(@GetUser() user: User) {
     return this.memoService.findAll(user.id);
   }
