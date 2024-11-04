8e7636feceba5a2e67b372703d8a164b45e5b903
raipen <raipen@knu.ac.kr>
2024-11-05 01:20:04 +0900

    feat: add Swagger annotations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,38 @@
-import { Controller, Get, Post, Request, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Headers } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Public } from 'src/auth/decorators/public';
+import { ApiBearerAuth, ApiConflictResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { GetUser } from 'src/auth/decorators/user';
+import { User } from './entities/user.entity';
 
 @Controller('user')
+@ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
   @Public()
+  @ApiOperation({
+    summary: '회원가입',
+    description: `firebase 토큰을 전달받아 회원가입을 진행합니다.\n
+      백엔드의 환경변수 중 NODE_ENV가 'test'이거나 'development'인 경우에는 토큰을 검증하지 않고 테스트용 유저를 생성하며,\n
+      회원가입을 진행하지 않고 다른 API를 테스트해도 생성되므로 굳이 회원가입을 진행하지 않아도 됩니다.
+    `
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiConflictResponse({ description: 'User already exists' })
+  @ApiCreatedResponse({ description: '회원가입 성공', type: User })
   signUp(@Headers('authorization') authorization: string) {
     return this.userService.create(authorization);
   }
 
   @Get()
-  findOne(@Request() req) {
-    return req.user;
+  @ApiBearerAuth()
+  @ApiOperation({ summary: '내 정보 조회', description: '내 정보를 조회합니다.' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiOkResponse({ description: '내 정보 조회 성공', type: User })
+  findOne(@GetUser() user:User) {
+    return user;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, OnModuleInit, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
@@ -10,6 +10,9 @@ export class UserService implements OnModuleInit {
   authService: AuthService;
   constructor(@InjectModel(User.name) private userModel: Model<User>, private readonly moduleRef: ModuleRef) {}
 
+  // AuthService를 주입받기 위해 OnModuleInit 인터페이스를 구현
+  // AuthService도 UserService에서 사용하기 때문에 모듈이 초기화된 후에 AuthService를 주입받기 위함
+  // 생성자로 AuthService를 주입받으면 순환 의존성 문제가 발생할 수 있음
   onModuleInit() {
     this.authService = this.moduleRef.get(AuthService, { strict: false });
   }
@@ -17,7 +20,9 @@ export class UserService implements OnModuleInit {
   async create(token: string) {
     token = token.replace('Bearer ', '');
     
-    const firebaseUser = await this.authService.verifyIdToken(token);
+    const firebaseUser = await this.authService.verifyIdToken(token).catch((e) => {
+      throw new UnauthorizedException();
+    });
     const user = await this.userModel.findOne({
       uid: firebaseUser.uid,
     }).exec();
